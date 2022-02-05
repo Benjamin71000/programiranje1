d5d294ba10cb997b790f3d5ae61361a94f50473d
Benjamin71000 <96847840+Benjamin71000@users.noreply.github.com>
2022-02-05 11:10:57 +0100

    Add files via upload

---
+++ zadatak5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int obrniBroj(int);
+
+int main() 
+{
+	int A, B;
+	while (cout << "Unesite A: ", cin >> A, A < 100000);
+	
+	B = obrniBroj(A);
+	cout << "A = " << A;
+	cout << "B = " << B;
+}
+
+int obrniBroj(int num)
+{
+	int trenutna_cifra = 0;
+	int novi_broj = 0;
+
+	while (num > 0)
+	{
+		trenutna_cifra = num % 10;
+		if (trenutna_cifra % 2 == 0)
+			num = num / 10;
+		else 
+		{
+			novi_broj = novi_broj * 10 + trenutna_cifra;
+			num = num / 10;
+		}
+	}
+	return novi_broj;
+}
